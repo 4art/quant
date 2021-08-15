0e806dbc02a27fad928b5b13c51e2c583a25b6ba
metraf <firsov.tyoma@gmail.com>
2021-08-15 13:38:38 +0200

    code formatting

--- time_value_of_money/time_value.py
+++ time_value_of_money/time_value.py
@@ -1,23 +1,28 @@
 from math import exp
 
+
 def future_discrete_value(x, r, n):
-	return x*(1+r)**n
+    return x*(1+r)**n
+
 
 def present_discrete_value(x, r, n):
- 	return x*(1+r)**-n
+    return x*(1+r)**-n
+
 
 def future_continuous_value(x, r, t):
-	return x*exp(r*t)
+    return x*exp(r*t)
+
 
 def present_continuous_value(x, r, t):
-	return x*exp(-r*t)
+    return x*exp(-r*t)
+
 
 if __name__ == "__main__":
-	x = 100
-	r = 0.05
-	n = 5
+    x = 100
+    r = 0.05
+    n = 5
 
-	print("future discrete value of x: %s" % future_discrete_value(x, r, n))
-	print("preset discrete value of x: %s" % present_discrete_value(x, r, n))
-	print("future continious value of x: %s" % future_continuous_value(x, r, n))
-	print("present continuous value of x: %s" % present_continuous_value(x, r, n))
+    print("future discrete value of x: %s" % future_discrete_value(x, r, n))
+    print("preset discrete value of x: %s" % present_discrete_value(x, r, n))
+    print("future continious value of x: %s" % future_continuous_value(x, r, n))
+    print("present continuous value of x: %s" %	present_continuous_value(x, r, n))
